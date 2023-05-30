197ccf1b18f1fb8d1db1e24628f1825b9963f797
AntiChange <chr1sf4ng@gmail.com>
2023-05-29 21:06:22 -0400

    Add requires

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 import "./style.css";
 import Experience from "./Experience/Experience.js";
 
+require('./skrollr.js')
+require('./sweetalert2.all.min.js')
+
 // Start ThreeJS
 const experience = new Experience(document.querySelector("canvas.webgl"));
